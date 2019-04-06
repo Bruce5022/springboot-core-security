5da80eb2822402abeb2a5078303804f4fad7d89b
szw <shizhanwei123@aliyun.com>
2019-04-06 17:45:12 +0800

    动态加载用户信息

---
+++ src/main/java/com/sky/service/SkyUserDetailService.java
@@ -0,0 +1,45 @@
+package com.sky.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sky.entity.Permission;
+import com.sky.entity.User;
+import com.sky.mapper.UserMapper;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SkyUserDetailService implements UserDetailsService {
+
+    private Logger logger = Logger.getLogger(SkyUserDetailService.class);
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 查询用户
+        User user = userMapper.findByUsername(username);
+        // 查询用户权限
+        List<Permission> permissions = userMapper.findPermissionByUsername(username);
+        List<GrantedAuthority> authoritys = new ArrayList<GrantedAuthority>();
+        for (Permission permission : permissions) {
+            GrantedAuthority authority = new SimpleGrantedAuthority(permission.getPermTag());
+            authoritys.add(authority);
+        }
+        user.setAuthorities(authoritys);
+        try {
+            logger.info("用户:"+new ObjectMapper().writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,10 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>登陆页面</title>
 </head>
 <body>
 <h1 style="color: blueviolet">登陆页面</h1>
+<c:if test="${not empty param.error}">
+    <font color="#8b0000">用户名或密码错误!</font>
+</c:if>
 <form action="${pageContext.request.contextPath}/userlogin" method="post">
     用户名:<input type="text" name="username"/><br/>
     姓名:<input type="password" name="password"/>
